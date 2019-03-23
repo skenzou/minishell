89f88074407644c2aab7647861624c8a9d43d98b
Mohamed Idrissi <midrissi@w-r6-p1.s19.be>
2019-03-23 22:53:16 +0100

    xD

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: midrissi <midrissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/22 21:38:26 by midrissi          #+#    #+#             */
-/*   Updated: 2019/03/22 21:43:57 by midrissi         ###   ########.fr       */
+/*   Updated: 2019/03/23 21:28:31 by midrissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,7 @@
 # include <fcntl.h>
 # include <pthread.h>
 # include <sys/wait.h>
+# define ANSI_CYAN		"\x1b[1m\x1b[36m"
+# define ANSI_RESET		"\x1b[0m"
 
 #endif
--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,36 +6,61 @@
 /*   By: midrissi <midrissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/08 09:15:55 by midrissi          #+#    #+#             */
-/*   Updated: 2019/03/23 18:26:25 by midrissi         ###   ########.fr       */
+/*   Updated: 2019/03/23 22:52:17 by midrissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int main(int argc, char **argv)
+void	print_curr_dir(void)
 {
-  pid_t father;
-  int a;
-  char *input;
-  int ret;
+	char	*str;
+	int		i;
+	int		j;
 
-  ret = 1;
-  while (ret == 1)
-  {
-    if ((ret = get_next_line(0, &input)) > 0)
-    {
-      father = fork();
-      if (father > 0)
-        wait(&a);
-      if (father == 0)
-        execve(input, argv, NULL);
-    }
-  }
-  (void)argc;
-    // father = fork();
-    // if (father > 0)
-    //   wait(&a);
-    //
-    // if (father == 0)
-    //     execve("/bin/ls", argv, NULL);
+	i = 0;
+	j = 0;
+	str = getcwd(NULL, 0);
+	while (str[i])
+	{
+		if (str[i] == '/' && str[i + 1])
+			j = i + 1;
+		i++;
+	}
+	ft_printf(ANSI_CYAN "-> %s$ " ANSI_RESET, str + j);
+	ft_strdel(&str);
+}
+
+int		main(int argc, char **argv, char **env)
+{
+	pid_t	father;
+	// int		a;
+	char	*input;
+	int		ret;
+	char	**args;
+
+	ret = 1;
+	int i = -1;
+	while (env[++i] != NULL)
+		ft_printf("%s\n", env[i]);
+	print_curr_dir();
+	while (ret == 1)
+	{
+		if ((ret = get_next_line(0, &input)) > 0)
+		{
+			father = fork();
+			if (father > 0)
+			{
+				wait(&father);
+				print_curr_dir();
+			}
+			if (father == 0)
+			{
+				args = ft_strsplit(input, ' ');
+				execve(args[0], args, NULL);
+			}
+		}
+	}
+	(void)argc;
+	(void)argv;
 }
