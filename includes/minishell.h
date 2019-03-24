c0ae414a0ecac4f6ca41d80bc32b169f3642fdd5
Mohamed Idrissi <skenz@Mohameds-iMac.local>
2019-03-24 01:00:50 +0100

    minishell

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: midrissi <midrissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/22 21:38:26 by midrissi          #+#    #+#             */
-/*   Updated: 2019/03/23 21:28:31 by midrissi         ###   ########.fr       */
+/*   Updated: 2019/03/24 01:00:41 by midrissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,10 @@
 # include <fcntl.h>
 # include <pthread.h>
 # include <sys/wait.h>
+# include <sys/types.h>
+# include <sys/stat.h>
 # define ANSI_CYAN		"\x1b[1m\x1b[36m"
 # define ANSI_RESET		"\x1b[0m"
+# define MAX_PATH_LEN	4096
 
 #endif
--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: midrissi <midrissi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/08 09:15:55 by midrissi          #+#    #+#             */
-/*   Updated: 2019/03/23 22:52:17 by midrissi         ###   ########.fr       */
+/*   Updated: 2019/03/24 01:00:39 by midrissi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,18 +31,70 @@ void	print_curr_dir(void)
 	ft_strdel(&str);
 }
 
+int		is_path(char *str)
+{
+	while (*str)
+	{
+		if (*str == '/')
+			return (1);
+		str++;
+	}
+	return (0);
+}
+
+char	**get_paths(char **env)
+{
+	int i;
+
+	i = -1;
+	while (env[++i])
+	{
+		if (!ft_strncmp(env[i], "PATH=", 5))
+			return (ft_strsplit(env[i] + 5, ':'));
+	}
+	return (NULL);
+}
+
+void		set_path(char fullpath[], char *cmd, char *path)
+{
+	ft_bzero(fullpath, MAX_PATH_LEN);
+	ft_strcpy(fullpath, path);
+	ft_strcat(fullpath, "/");
+	ft_strcat(fullpath, cmd);
+}
+
+void		get_bin(char *cmd, char **env, char **args)
+{
+	char **paths;
+	char path[MAX_PATH_LEN];
+	struct stat buf;
+	int i;
+
+	paths = get_paths(env);
+	i = -1;
+	while (paths[++i])
+	{
+			set_path(path, cmd, paths[i]);
+			if (!lstat(path, &buf))
+			{
+				if (!access(path, X_OK))
+				{
+					execve(path, args, env);
+					return ;
+				}
+			}
+	}
+}
+
 int		main(int argc, char **argv, char **env)
 {
 	pid_t	father;
-	// int		a;
+	 int		a;
 	char	*input;
 	int		ret;
 	char	**args;
 
 	ret = 1;
-	int i = -1;
-	while (env[++i] != NULL)
-		ft_printf("%s\n", env[i]);
 	print_curr_dir();
 	while (ret == 1)
 	{
@@ -51,13 +103,18 @@ int		main(int argc, char **argv, char **env)
 			father = fork();
 			if (father > 0)
 			{
-				wait(&father);
+				wait(&a);
 				print_curr_dir();
 			}
 			if (father == 0)
 			{
 				args = ft_strsplit(input, ' ');
-				execve(args[0], args, NULL);
+				if (is_path(args[0]))
+					execve(args[0], args, env);
+				else
+				{
+						get_bin(args[0], env, args);
+				}
 			}
 		}
 	}
